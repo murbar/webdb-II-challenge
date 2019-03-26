0ef882abd6d80e85f604ea764f79c68bfa8f91ed
Joel Bartlett <joel@murbar.com>
2019-03-26 16:08:57 -0400

    Config knex

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3'
+  }
+};
+
+const db = knex(knexConfig);
 const server = express();
 
 server.use(express.json());
