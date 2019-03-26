de06963de8949bd3dd539a449d5b080bc5465411
Joel Bartlett <joel@murbar.com>
2019-03-26 16:09:56 -0400

    Add GET zoos

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json({ error: 'Cannot get zoos.' });
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
